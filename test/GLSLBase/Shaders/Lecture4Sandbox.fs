da6eb98bec05f2bb7e516a9adf8cb409c1ddb653
Iconer <yoyofa2@hotmail.com>
2022-04-30 14:55:47 +0900

    Week 8-1

--- test/GLSLBase/Shaders/Lecture4Sandbox.fs
+++ test/GLSLBase/Shaders/Lecture4Sandbox.fs
@@ -2,18 +2,37 @@
 
 layout(location=0) out vec4 FragColor;
 
-varying vec3 v_Position;
-
+in vec3 v_Position;
 in vec4 v_Colour;
+
+vec4 DrawEdgedCircle(float outer_radius, float thickness);
 
 void main()
 {
-	vec3 circle_surface = v_Position - vec3(0.5f);
-	float distance = length(circle_surface);
+	vec2 circle_surface = v_Position.xy;
+	float radius = length(circle_surface);
 
-	float radius = 0.4f;
-	float smoothness = 0.02f;
+	// float dist = distance(v_Position.xy, vec(0.5f));
 
-	vec4 circle = vec4(vec3(smoothstep(radius, radius - smoothness, 0.39f)), 1.0f);
-	FragColor = v_Colour * circle;
+	vec4 circle_color = vec4(0.0f);
+	if (radius < 0.4f)
+	{
+		circle_color = vec4(1.0f);
+	}
+
+	//float radius = 0.4f;
+	//float smoothness = 0.02f;
+	//circle_color = vec4(vec3(smoothstep(radius, radius - smoothness, 0.39f)), 1.0f);
+
+	circle_color = DrawEdgedCircle(0.4f, 0.02f);
+	FragColor = v_Colour * circle_color;
+}
+
+vec4 DrawEdgedCircle(float outer_radius, float thickness)
+{
+	float inner_radius = outer_radius - thickness;
+
+	float radius = distance(v_Position.xy, vec2(0.0f));
+
+	return vec4(vec3(smoothstep(inner_radius, outer_radius, radius)), 1.0f);
 }
